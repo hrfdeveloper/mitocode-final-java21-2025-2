802414975dee17a8123d42283a559a0536d8dce9
Henry Ramirez Fano <hramirez@mincetur.gob.pe>
2025-06-08 18:28:05 -0500

    Repository layer creation

---
+++ src/main/java/com/mitocode/academy/repository/ICourseRepo.java
@@ -0,0 +1,6 @@
+package com.mitocode.academy.repository;
+
+import com.mitocode.academy.model.Course;
+
+public interface ICourseRepo extends IGenericRepo<Course,Integer>{
+}
---
+++ src/main/java/com/mitocode/academy/repository/IStudentRepo.java
@@ -0,0 +1,6 @@
+package com.mitocode.academy.repository;
+
+import com.mitocode.academy.model.Student;
+
+public interface IStudentRepo extends IGenericRepo<Student,Integer>{
+}
