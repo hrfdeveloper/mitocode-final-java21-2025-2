3ecb65c08adead3ce497e115246b95b29c01fc65
Henry Ramirez Fano <hramirez@mincetur.gob.pe>
2025-06-08 18:28:49 -0500

    Service layer interfaces creation

---
+++ src/main/java/com/mitocode/academy/service/ICourseService.java
@@ -0,0 +1,6 @@
+package com.mitocode.academy.service;
+
+import com.mitocode.academy.model.Course;
+
+public interface ICourseService extends ICRUD<Course,Integer> {
+}
---
+++ src/main/java/com/mitocode/academy/service/IStudentService.java
@@ -0,0 +1,6 @@
+package com.mitocode.academy.service;
+
+import com.mitocode.academy.model.Student;
+
+public interface IStudentService extends ICRUD<Student,Integer> {
+}
